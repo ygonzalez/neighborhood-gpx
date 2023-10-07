ba1174a2eb8800dcc6c937d94c8d02ed06cfca62
ygonzalez <ygonzalez704@gmail.com>
2023-10-07 13:18:56 -0500

    add setup

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="your_project_name",
+    name="neighborhood-gpx",
     version="0.1",
-    packages=find_packages(),
+    # packages=find_packages(),
     install_requires=[
-    "osmnx==1.3.0",
     "geopandas==0.14.0",
     "networkx>=2.4",
     "matplotlib>=3.2.1",
